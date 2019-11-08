9c29ffc98de89152eb74f1f4352a3eeb1cc3be06
telliex <telliexyuzo@gmail.com>
2019-11-08 13:32:22 +0800

    docs(README.md/.commitlintrc.js): 调整项目说明文字 ＆ 修改 commitlint
    1.修改 README.md 说明
    2.调整 .commitlintrc.js rules

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,4 +1,9 @@
 module.exports={
   extends: ['@commitlint/config-conventional'],
-  rules: {}
+  rules: {
+    'header-max-length': [0, "always", 72],
+    'header-case': [0, 'never'],
+    'scope-case': [0, 'never'],
+  }
 };
+// rules reference : https://commitlint.js.org/#/reference-rules
