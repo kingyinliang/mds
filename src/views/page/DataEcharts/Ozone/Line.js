2818b4deb5b58a8dda391ac01f6a4420fa007e32
kingyinliang <wangyinliang@shinho.net.cn>
2019-12-18 16:14:20 +0800

    feat(juice): add juice form

--- src/views/page/DataEcharts/Ozone/Line.js
+++ src/views/page/DataEcharts/Ozone/Line.js
@@ -1,66 +1,102 @@
-
-let data = [['2000-06-05', 116], ['2000-06-06', 129], ['2000-06-07', 135], ['2000-06-08', 86], ['2000-06-09', 73], ['2000-06-10', 85], ['2000-06-11', 73], ['2000-06-12', 68], ['2000-06-13', 92], ['2000-06-14', 130], ['2000-06-15', 245], ['2000-06-16', 139], ['2000-06-17', 115], ['2000-06-18', 111], ['2000-06-19', 309], ['2000-06-20', 206], ['2000-06-21', 137], ['2000-06-22', 128], ['2000-06-23', 85], ['2000-06-24', 94], ['2000-06-25', 71], ['2000-06-26', 106], ['2000-06-27', 84], ['2000-06-28', 93], ['2000-06-29', 85], ['2000-06-30', 73], ['2000-07-01', 83], ['2000-07-02', 125], ['2000-07-03', 107], ['2000-07-04', 82], ['2000-07-05', 44], ['2000-07-06', 72], ['2000-07-07', 106], ['2000-07-08', 107], ['2000-07-09', 66], ['2000-07-10', 91], ['2000-07-11', 92], ['2000-07-12', 113], ['2000-07-13', 107], ['2000-07-14', 131], ['2000-07-15', 111], ['2000-07-16', 64], ['2000-07-17', 69], ['2000-07-18', 88], ['2000-07-19', 77], ['2000-07-20', 83], ['2000-07-21', 111], ['2000-07-22', 57], ['2000-07-23', 55], ['2000-07-24', 60]]
-
-var dateList = data.map(function (item) {
-  return item[0]
-})
-var valueList = data.map(function (item) {
-  return item[1]
-})
 export let line = {
-
-  // Make gradient line here
-  visualMap: [{
-    show: false,
-    type: 'continuous',
-    seriesIndex: 0,
-    min: 0,
-    max: 400
-  }, {
-    show: false,
-    type: 'continuous',
-    seriesIndex: 1,
-    dimension: 0,
-    min: 0,
-    max: dateList.length - 1
-  }],
   title: [{
-    left: 'center',
-    text: 'Gradient along the y axis'
-  }, {
-    top: '55%',
-    left: 'center',
-    text: 'Gradient along the x axis'
+    left: '15px',
+    top: '15px',
+    textStyle: {
+      color: '#fff',
+      fontSize: '18px'
+    },
+    text: '三楼更衣室浓度'
   }],
-  tooltip: {
-    trigger: 'axis'
+  xAxis: {
+    axisLine: {
+      lineStyle: {
+        color: '#2D4288'
+      }
+    },
+    axisTick: {
+      show: false
+    },
+    axisLabel: {
+      show: false
+    },
+    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
   },
-  xAxis: [{
-    data: dateList
-  }, {
-    data: dateList,
-    gridIndex: 1
-  }],
-  yAxis: [{
-    splitLine: {show: false}
-  }, {
-    splitLine: {show: false},
-    gridIndex: 1
-  }],
-  grid: [{
-    bottom: '60%'
-  }, {
-    top: '60%'
-  }],
+  yAxis: {
+    type: 'value',
+    axisLine: {
+      lineStyle: {
+        color: '#2D4288'
+      }
+    },
+    axisTick: {
+      show: false
+    },
+    axisLabel: {
+      color: '#fff',
+      fontSize: '12px'
+    },
+    splitLine: {
+      show: false
+    }
+  },
+  grid: {
+    left: '40px',
+    right: '20px',
+    bottom: '30px'
+  },
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      type: 'cross',
+      label: {
+        backgroundColor: '#6a7985'
+      }
+    }
+  },
   series: [{
+    data: [820, 932, 901, 934, 1290, 1330, 1320],
+    // data: [0, 0, 0, 0, 0, 0, 0],
     type: 'line',
     showSymbol: false,
-    data: valueList
-  }, {
-    type: 'line',
-    showSymbol: false,
-    data: valueList,
-    xAxisIndex: 1,
-    yAxisIndex: 1
+    smooth: true,
+    lineStyle: {
+      color: {
+        type: 'linear',
+        x: 0,
+        y: 0,
+        x2: 1,
+        y2: 0,
+        colorStops: [{
+          offset: 0, color: '#00FE9D'
+        }, {
+          offset: 0.33, color: '#02BFF1'
+        }, {
+          offset: 0.66, color: '#339CFF'
+        }, {
+          offset: 1, color: '#917FFF'
+        }],
+        globalCoord: false
+      }
+    },
+    areaStyle: {
+      color: {
+        type: 'linear',
+        x: 0,
+        y: 0,
+        x2: 1,
+        y2: 0,
+        colorStops: [{
+          offset: 0, color: 'rgba(0, 254, 157, 0.6)'
+        }, {
+          offset: 0.33, color: 'rgba(2, 191, 241, 0.6)'
+        }, {
+          offset: 0.66, color: 'rgba(51, 156, 255, 0.6)'
+        }, {
+          offset: 1, color: 'rgba(145, 127, 255, 0.6)'
+        }],
+        globalCoord: false
+      }
+    }
   }]
 }
