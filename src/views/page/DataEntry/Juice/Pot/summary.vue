dbda1e551d7b5ca912f02e9af532d3d6d586004f
16080029 <maxuehan@shinho.net.cn>
2019-12-30 10:33:12 +0800

    feat(): 原汁库存汇报表+臭氧报表

--- src/views/page/DataEntry/Juice/Pot/summary.vue
+++ src/views/page/DataEntry/Juice/Pot/summary.vue
@@ -119,41 +119,29 @@
           <i class="iconfont factory-kucun" style="color: #666; margin-right: 10px;"></i>原汁库存汇总表（单位：方）
           <el-button type="primary" size="small" @click="ExportExcelA(true)"  v-if="isAuth('juice:pot:juiceStockItemExport')" style="background-color: #1890ff; color: #fff; float: right;">导出</el-button>
         </div>
-        <el-table header-row-class-name="tableHead" :data="dataLeftListOrder" border tooltip-effect="dark" >
-          <el-table-column label="" :show-overflow-tooltip="true" prop="holderName"></el-table-column>
-          <el-table-column label=">=7天" :show-overflow-tooltip="true" prop="holderName" width="120"></el-table-column>
-          <el-table-column label="6天" :show-overflow-tooltip="true" width="75">
-            <template slot-scope="scope">
-              {{scope.row.holderStatus === '6' ? '空罐' : scope.row.holderStatus === '7' ? '入料中' : scope.row.holderStatus === '8' ? '沉淀中' : scope.row.holderStatus === '9' ? '领用中' : scope.row.holderStatus === '10' ? '待清洗' : ''}}
-            </template>
-          </el-table-column>
-          <el-table-column label="5天" :show-overflow-tooltip="true" prop="amount" width="100"></el-table-column>
-          <el-table-column label="4天" :show-overflow-tooltip="true" prop="fermentationHolderName" width="100"></el-table-column>
-          <el-table-column label="3天" :show-overflow-tooltip="true" prop="fullDate" width="160"></el-table-column>
-          <el-table-column label="2天" :show-overflow-tooltip="true" prop="workShop" width="70"></el-table-column>
-          <el-table-column label="1天" :show-overflow-tooltip="true" width="160">
-            <template slot-scope="scope">
-              {{scope.row.materialCode}}{{scope.row.materialName}}
-            </template>
-          </el-table-column>
-          <el-table-column label="0天" :show-overflow-tooltip="true" prop="type" width="70"></el-table-column>
-          <el-table-column label="合计" :show-overflow-tooltip="true" width="80">
-            <template slot-scope="scope">
-              {{scope.row.isF === '1' ? 'F0' : scope.row.isF === '2' ? 'JBS' : ''}}
-            </template>
-          </el-table-column>
+        <el-table header-row-class-name="tableHead" :data="formDataTop" border tooltip-effect="dark" >
+          <el-table-column label="" :show-overflow-tooltip="true" prop="type"></el-table-column>
+          <el-table-column label=">=7天" :show-overflow-tooltip="true" prop="gtSevenDay" ></el-table-column>
+          <el-table-column label="6天" :show-overflow-tooltip="true" prop="sixDay"></el-table-column>
+          <el-table-column label="5天" :show-overflow-tooltip="true" prop="fiveDay"></el-table-column>
+          <el-table-column label="4天" :show-overflow-tooltip="true" prop="fourDay"></el-table-column>
+          <el-table-column label="3天" :show-overflow-tooltip="true" prop="threeDay"></el-table-column>
+          <el-table-column label="2天" :show-overflow-tooltip="true" prop="twoDay"></el-table-column>
+          <el-table-column label="1天" :show-overflow-tooltip="true" prop="oneDay"></el-table-column>
+          <el-table-column label="0天" :show-overflow-tooltip="true" prop="zeroDay"></el-table-column>
+          <el-table-column label="合计" :show-overflow-tooltip="true" prop="sumAmount"></el-table-column>
         </el-table>
-        <el-table header-row-class-name="tableHead" :data="formLeftData" border tooltip-effect="dark" style="margin-top: 25px;">
-          <el-table-column label="" :show-overflow-tooltip="true" prop="liuyuexian"></el-table-column>
-          <el-table-column label=">=15天" :show-overflow-tooltip="true" prop="weijixian"></el-table-column>
-          <el-table-column label="14天" :show-overflow-tooltip="true" prop="f0"></el-table-column>
-          <el-table-column label="13天" :show-overflow-tooltip="true" prop="jy"></el-table-column>
-          <el-table-column label="12天" :show-overflow-tooltip="true" prop="tb"></el-table-column>
-          <el-table-column label="11天" :show-overflow-tooltip="true" prop="jbs"></el-table-column>
-          <el-table-column label="10天" :show-overflow-tooltip="true" prop="jytb"></el-table-column>
-          <el-table-column label="9天" :show-overflow-tooltip="true" prop="sum" ></el-table-column>
-          <el-table-column label="<9天" :show-overflow-tooltip="true" prop="sum" ></el-table-column>
-          <el-table-column label="合计" :show-overflow-tooltip="true" prop="sum" ></el-table-column>
+        <el-table header-row-class-name="tableHead" :data="formData" border tooltip-effect="dark" style="margin-top: 25px;">
+          <el-table-column label="" :show-overflow-tooltip="true" prop="type"></el-table-column>
+          <el-table-column label=">=15天" :show-overflow-tooltip="true" prop="gtFifteenDay"></el-table-column>
+          <el-table-column label="14天" :show-overflow-tooltip="true" prop="fourteenDay"></el-table-column>
+          <el-table-column label="13天" :show-overflow-tooltip="true" prop="thirteenDay"></el-table-column>
+          <el-table-column label="12天" :show-overflow-tooltip="true" prop="twelveDay"></el-table-column>
+          <el-table-column label="11天" :show-overflow-tooltip="true" prop="elevenDay"></el-table-column>
+          <el-table-column label="10天" :show-overflow-tooltip="true" prop="tenDay"></el-table-column>
+          <el-table-column label="9天" :show-overflow-tooltip="true" prop="nineDay" ></el-table-column>
+          <el-table-column label="<9天" :show-overflow-tooltip="true" prop="ltNineDay" ></el-table-column>
+          <el-table-column label="合计" :show-overflow-tooltip="true" prop="sumAmount" ></el-table-column>
         </el-table>
       </el-tab-pane>
     </el-tabs>
@@ -180,7 +168,9 @@ export default {
       dataPageSizeRight: 10,
       dataRightCurrPage: 1,
       dataRightTotalCount: 0,
-      plantList: {}
+      plantList: {},
+      formDataTop: [],
+      formData: []
     }
   },
   mounted () {
@@ -206,6 +196,19 @@ export default {
           this.changeRightDataList()
         } else {
           this.$notify.error({title: '错误', message: data.msg})
+        }
+      })
+      this.$http(`${JUICE_API.JUICE_POT_STOCKSUM}`, 'POST', {}).then(({data}) => {
+        if (data.code === 0) {
+          data.getJuiceStork.map((item) => {
+            if (item.type === 'F0' || item.type === 'JBS') {
+              this.formData.push(item)
+            } else {
+              this.formDataTop.push(item)
+            }
+          })
+        } else {
+          this.$error_SHINHO(data.msg)
         }
       })
     },
