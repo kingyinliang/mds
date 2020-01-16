7c30a9cd307b7a1d92711c28f2f1b0190a9324ea
18060064 <Wyl945521?>
2020-01-16 19:11:38 +0800

    feat(login): login 动画

--- src/views/common/loginCanvas.js
+++ src/views/common/loginCanvas.js
@@ -8,24 +8,30 @@ export class LoginAnimation {
     this.canvas.height = this.height
   }
   init () {
-    let num = 1
+    let windmillNum = 1
+    let carNum = 1
     let bg = new Image()
     bg.src = './static/img/loginBg.png'
     bg.onload = () => {
       let windmill = new Image()
       windmill.src = './static/img/windmill.png'
       windmill.onload = () => {
-        console.log('start')
-        setInterval(() => {
-          this.ctx.save()
-          this.ctx.clearRect(0, 0, this.width, this.height)
-          this.ctx.drawImage(bg, 0, 0, this.width, this.height)
-          this.ctx.translate((this.width / 253) + (this.width / 6.4) / 2, this.height / 5.04 + (this.width / 6.4) / 2)
-          this.ctx.rotate(num * Math.PI / 180)
-          this.ctx.drawImage(windmill, -((this.width / 6.4) / 2), -((this.width / 6.4) / 2), this.width / 6.4, this.width / 6.4)
-          this.ctx.restore()
-          num++
-        }, 33)
+        let car = new Image()
+        car.src = './static/img/car.png'
+        car.onload = () => {
+          setInterval(() => {
+            this.ctx.save()
+            this.ctx.clearRect(0, 0, this.width, this.height)
+            this.ctx.drawImage(bg, 0, 0, this.width, this.height)
+            this.ctx.translate((this.width / 253) + (this.width / 6.4) / 2, this.height / 5.04 + (this.width / 6.4) / 2)
+            this.ctx.rotate(windmillNum * Math.PI / 180)
+            this.ctx.drawImage(windmill, -((this.width / 6.4) / 2), -((this.width / 6.4) / 2), this.width / 6.4, this.width / 6.4)
+            this.ctx.restore()
+            this.ctx.drawImage(car, (this.width / 2.05) - carNum, (this.height / 2.6) + carNum / 1.6, this.width / 10, this.width / 10)
+            windmillNum++
+            carNum = carNum + 2
+          }, 33)
+        }
       }
     }
   }
