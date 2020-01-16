c29055a09ecc7698e5e99193d1ae893958d21010
16080029 <maxuehan@shinho.net.cn>
2020-01-16 17:06:27 +0800

    fix(): 过滤入库罐体NaN

--- src/views/page/DataEntry/Filtration/DataEntry/common/inStorage.vue
+++ src/views/page/DataEntry/Filtration/DataEntry/common/inStorage.vue
@@ -175,7 +175,7 @@ export default {
           this.InStorageDate = data.list
           if (this.InStorageDate.length > 0) {
             this.PotDetail = {
-              amount: accAdd(this.InStorageDate[0].holderRemaining, this.InStorageDate[0].unit),
+              amount: this.InStorageDate[0].holderRemaining + this.InStorageDate[0].unit,
               batch: this.InStorageDate[0].batch,
               material: this.InStorageDate[0].materialCode + ' ' + this.InStorageDate[0].materialName
             }
@@ -260,7 +260,7 @@ export default {
           }, 500)
           if (this.InStorageDate.length > 0) {
             this.PotDetail = {
-              amount: accAdd(this.InStorageDate[0].holderRemaining, this.InStorageDate[0].unit),
+              amount: this.InStorageDate[0].holderRemaining + this.InStorageDate[0].unit,
               batch: this.InStorageDate[0].batch,
               material: this.InStorageDate[0].materialCode + ' ' + this.InStorageDate[0].materialName
             }
