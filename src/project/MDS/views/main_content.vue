45b17dd45f53e04f4387dbb7fcf691e3ac202e37
Telliex <telliexyuzo@gmail.com>
2020-04-14 21:19:49 +0800

    refactor(): 优化代码
    调整 css 宣告顺序

--- src/project/MDS/views/main_content.vue
+++ src/project/MDS/views/main_content.vue
@@ -43,67 +43,95 @@
 </template>
 
 <script>
-import { isURL } from '@/net/validate';
+    import { isURL } from '@/net/validate';
 
-export default {
-    data() {
-        return {};
-    },
-    computed: {
-        documentClientHeight: {
-            get() {
-                return this.$store.state.common.documentClientHeight;
-            }
+    export default {
+        data() {
+            return {};
         },
-        menuActiveName: {
-            get() {
-                return this.$store.state.common.menuActiveName;
+        computed: {
+            documentClientHeight: {
+                get() {
+                    return this.$store.state.common.documentClientHeight;
+                }
             },
-            set(val) {
-                this.$store.commit('common/updateMenuActiveName', val);
-            }
-        },
-        mainTabs: {
-            get() {
-                return this.$store.state.common.mainTabs;
+            menuActiveName: {
+                get() {
+                    return this.$store.state.common.menuActiveName;
+                },
+                set(val) {
+                    this.$store.commit('common/updateMenuActiveName', val);
+                }
             },
-            set(val) {
-                this.$store.commit('common/updateMainTabs', val);
-            }
-        },
-        mainTabsActiveName: {
-            get() {
-                return this.$store.state.common.mainTabsActiveName;
+            mainTabs: {
+                get() {
+                    return this.$store.state.common.mainTabs;
+                },
+                set(val) {
+                    this.$store.commit('common/updateMainTabs', val);
+                }
             },
-            set(val) {
-                this.$store.commit('common/updateMainTabsActiveName', val);
+            mainTabsActiveName: {
+                get() {
+                    return this.$store.state.common.mainTabsActiveName;
+                },
+                set(val) {
+                    this.$store.commit('common/updateMainTabsActiveName', val);
+                }
+            },
+            siteContentViewHeight() {
+                let height = this.documentClientHeight - 50 - 30 - 2;
+                if (this.$route.meta.isTab) {
+                    height -= 40;
+                    return isURL(this.$route.meta.iframeUrl) ? { height: height + 'px' } : { minHeight: height + 'px' };
+                }
+                return { minHeight: height + 'px' };
             }
         },
-        siteContentViewHeight() {
-            let height = this.documentClientHeight - 50 - 30 - 2;
-            if (this.$route.meta.isTab) {
-                height -= 40;
-                return isURL(this.$route.meta.iframeUrl) ? { height: height + 'px' } : { minHeight: height + 'px' };
-            }
-            return { minHeight: height + 'px' };
-        }
-    },
-    methods: {
-        // tabs, 选中tab
-        selectedTabHandle(tab) {
-            const finalTab = this.mainTabs.filter(item => item.name === tab.name);
-            if (finalTab.length >= 1 && this.$router.history.current.name !== finalTab[0].name) {
-                this.$router.push({ name: finalTab[0].name });
-            }
-        },
-        // tabs, 删除tab
-        removeTabHandle(tabName, add) {
-            if (/录入/g.test(this.mainTabs.filter(item => item.name === tabName)[0].title)) {
-                this.$confirm('为防止数据丢失请保存后关闭, 是否继续?', '关闭', {
-                    confirmButtonText: '确定',
-                    cancelButtonText: '取消',
-                    type: 'warning'
-                }).then(() => {
+        methods: {
+            // tabs, 选中tab
+            selectedTabHandle(tab) {
+                const finalTab = this.mainTabs.filter(item => item.name === tab.name);
+                if (finalTab.length >= 1 && this.$router.history.current.name !== finalTab[0].name) {
+                    this.$router.push({ name: finalTab[0].name });
+                }
+            },
+            // tabs, 删除tab
+            removeTabHandle(tabName, add) {
+                if (/录入/g.test(this.mainTabs.filter(item => item.name === tabName)[0].title)) {
+                    this.$confirm('为防止数据丢失请保存后关闭, 是否继续?', '关闭', {
+                        confirmButtonText: '确定',
+                        cancelButtonText: '取消',
+                        type: 'warning'
+                    })
+                        .then(() => {
+                            this.mainTabs = this.mainTabs.filter(item => item.name !== tabName);
+                            if (this.mainTabs.length >= 1) {
+                                // 当前选中tab被删除
+                                if (tabName === this.mainTabsActiveName) {
+                                    this.$router.push(
+                                        {
+                                            name: this.mainTabs[this.mainTabs.length - 1].name
+                                        },
+                                        () => {
+                                            this.mainTabsActiveName = this.$route.name;
+                                        }
+                                    );
+                                }
+                            } else {
+                                this.menuActiveName = '';
+                                this.$router.push({ name: 'home' });
+                            }
+                            if (add) {
+                                this.$nextTick(() => {
+                                    this.$router.push({ name: tabName });
+                                });
+                            }
+                        })
+                        .catch(() => {
+                            // this.$infoTost('已取消删除');
+                        });
+                } else {
                     this.mainTabs = this.mainTabs.filter(item => item.name !== tabName);
                     if (this.mainTabs.length >= 1) {
                         // 当前选中tab被删除
@@ -121,108 +149,81 @@ export default {
                         this.menuActiveName = '';
                         this.$router.push({ name: 'home' });
                     }
-                    if (add) {
-                        this.$nextTick(() => {
-                            this.$router.push({ name: tabName });
-                        });
-                    }
-                }).catch(() => {
-                // this.$infoTost('已取消删除');
-                });
-            } else {
-                this.mainTabs = this.mainTabs.filter(item => item.name !== tabName);
-                if (this.mainTabs.length >= 1) {
-                    // 当前选中tab被删除
-                    if (tabName === this.mainTabsActiveName) {
-                        this.$router.push(
-                            {
-                                name: this.mainTabs[this.mainTabs.length - 1].name
-                            },
-                            () => {
-                                this.mainTabsActiveName = this.$route.name;
-                            }
-                        );
-                    }
-                } else {
-                    this.menuActiveName = '';
-                    this.$router.push({ name: 'home' });
                 }
+            },
+            // tabs, 关闭当前
+            tabsCloseCurrentHandle() {
+                this.removeTabHandle(this.mainTabsActiveName);
+            },
+            // tabs, 关闭其它
+            tabsCloseOtherHandle() {
+                this.mainTabs = this.mainTabs.filter(item => item.name === this.mainTabsActiveName);
+            },
+            // tabs, 关闭全部
+            tabsCloseAllHandle() {
+                this.mainTabs = [];
+                this.menuActiveName = '';
+                this.$router.push({ name: 'home' });
+            },
+            // tabs, 刷新当前
+            tabsRefreshCurrentHandle() {
+                const tempTabName = this.mainTabsActiveName;
+                this.removeTabHandle(tempTabName, true);
+                this.$nextTick(() => {
+                    this.$router.push({ name: tempTabName });
+                });
             }
-        },
-        // tabs, 关闭当前
-        tabsCloseCurrentHandle() {
-            this.removeTabHandle(this.mainTabsActiveName);
-        },
-        // tabs, 关闭其它
-        tabsCloseOtherHandle() {
-            this.mainTabs = this.mainTabs.filter(item => item.name === this.mainTabsActiveName);
-        },
-        // tabs, 关闭全部
-        tabsCloseAllHandle() {
-            this.mainTabs = [];
-            this.menuActiveName = '';
-            this.$router.push({ name: 'home' });
-        },
-        // tabs, 刷新当前
-        tabsRefreshCurrentHandle() {
-            const tempTabName = this.mainTabsActiveName;
-            this.removeTabHandle(tempTabName, true);
-            this.$nextTick(() => {
-                this.$router.push({ name: tempTabName });
-            });
         }
-    }
-};
+    };
 </script>
 <style lang="scss" scoped>
-.footer {
-    text-align: center;
-    width: 100%;
-    position: absolute;
-    bottom: 55px;
-    color: rgba(0, 0, 0, 0.45);
-    line-height: 20px;
-    font-size: 12px;
-    a {
-        font-size: 14px;
+    .footer {
+        position: absolute;
+        bottom: 55px;
+        width: 100%;
         color: rgba(0, 0, 0, 0.45);
-        margin: 0 10px;
+        font-size: 12px;
+        line-height: 20px;
+        text-align: center;
+        a {
+            margin: 0 10px;
+            color: rgba(0, 0, 0, 0.45);
+            font-size: 14px;
+        }
     }
-}
 
-@keyframes rollin {
-    0% {
-        opacity: 0;
-        transform: translate3d(-100%, 0, 0) rotate(-0deg);
+    @keyframes rollin {
+        0% {
+            transform: translate3d(-100%, 0, 0) rotate(-0deg);
+            opacity: 0;
+        }
+        to {
+            transform: none;
+            opacity: 1;
+        }
     }
-    to {
-        opacity: 1;
-        transform: none;
+
+    .rollin {
+        animation-name: rollin;
     }
-}
+    .animated {
+        animation-duration: 1s;
+        animation-fill-mode: both;
+    }
 
-.rollin {
-    animation-name: rollin;
-}
-.animated {
-    animation-duration: 1s;
-    animation-fill-mode: both;
-}
+    .animated.infinite {
+        animation-iteration-count: infinite;
+    }
 
-.animated.infinite {
-    animation-iteration-count: infinite;
-}
+    .animated.hinge {
+        animation-duration: 2s;
+    }
 
-.animated.hinge {
-    animation-duration: 2s;
-}
-
-/* stylelint-disable */
-.animated.bounceIn,
-.animated.bounceOut,
-.animated.flipOutX,
-.animated.flipOutY {
-    animation-duration: 0.75s;
-}
-
+    /* stylelint-disable */
+    .animated.bounceIn,
+    .animated.bounceOut,
+    .animated.flipOutX,
+    .animated.flipOutY {
+        animation-duration: 0.75s;
+    }
 </style>
