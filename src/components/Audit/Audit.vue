5f3c750afb30828398f27909e11cf9fb0d4c806a
18060064 <Wyl945521?>
2020-04-28 11:36:16 +0800

    feat(404): 改样式添加组件和加404 500页面

---
+++ src/components/Audit/Audit.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        a
+    </div>
+</template>
+
+<script lang="ts">
+    import { Vue, Component } from 'vue-property-decorator';
+
+    @Component({
+        components: {
+        }
+    })
+    export default class Audit extends Vue {
+
+    }
+</script>
+
+<style scoped>
+
+</style>
