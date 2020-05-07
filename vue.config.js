d66a9eec53bf98bb0a24a9619544dc0f9c59da0a
18060064 <Wyl945521?>
2020-05-07 17:55:19 +0800

    feat(test): 菜单联调

--- vue.config.js
+++ vue.config.js
@@ -23,6 +23,15 @@ module.exports = {
                 pathRewrite: {
                     '^/dt': '/'
                 }
+            },
+            '/xj': {
+                target: 'http://10.10.22.233:8081/', // 目标代理接口地址
+                secure: false,
+                changeOrigin: true, // 开启代理，在本地创建一个虚拟服务端
+                // ws: true, // 是否启用websockets
+                pathRewrite: {
+                    '^/xj': '/'
+                }
             }
         }
     },
