05596f67606ea9761e36f921d3ecd4771c37a7d0
18060064 <Wyl945521?>
2020-06-01 18:06:04 +0800

    feat(test): test

---
+++ src/layout/main/main_leftbar_menu.vue
@@ -0,0 +1,71 @@
+<template>
+    <el-submenu v-if="!getChildren" :index="menu.id + ''" :popper-class="'site-sidebar--' + sidebarLayoutSkin + '-popper'">
+        <template slot="title">
+            <div @click="gotoRouteHandle(menu)">
+                <i :class="menu.menuIcon || ''" class="site-sidebar__menu-icon iconfont" />
+                <span>{{ menu.menuName }}</span>
+            </div>
+        </template>
+        <template v-for="item in menu.list">
+            <sub-menu v-if="item.menuType !== 'P'" :key="item.id" :menu="item" :dynamic-menu-routes="dynamicMenuRoutes" />
+        </template>
+    </el-submenu>
+    <el-menu-item v-else :index="menu.id + ''" @click="gotoRouteHandle(menu)">
+        <i :class="menu.menuIcon || ''" class="site-sidebar__menu-icon iconfont" />
+        <span>{{ menu.menuName }}</span>
+    </el-menu-item>
+</template>
+
+<script>
+export default {
+    name: 'MainLeftbarMenu',
+    components: {
+        SubMenu: resolve => {
+            require(['./main_leftbar_menu'], resolve);
+        }
+    },
+    props: {
+        menu: {
+            type: Object,
+            required: true
+        },
+        dynamicMenuRoutes: {
+            type: Array,
+            required: true
+        }
+    },
+    data() {
+        return {};
+    },
+    computed: {
+        sidebarLayoutSkin: {
+            get() {
+                return this.$store.state.common.sidebarLayoutSkin;
+            }
+        },
+        getChildren: function() {
+            let st = true;
+            if (this.menu.list && this.menu.list.length >= 1) {
+                st = true;
+                this.menu.list.forEach(item => {
+                    if (item.menuType !== 'P') {
+                        st = false;
+                    }
+                });
+            }
+            return st;
+        }
+    },
+    methods: {
+        // 通过menuId与动态(菜单)路由进行匹配跳转至指定路由
+        gotoRouteHandle(menu) {
+            const route = this.dynamicMenuRoutes.filter(item => item.meta.menuId === menu.id);
+            if (route.length >= 1) {
+                this.$router.push({ path: route[0].path });
+            }
+        }
+    }
+};
+</script>
+
+<style scoped></style>
---
+++ src/layout/main/main_topbar.vue
@@ -0,0 +1,153 @@
+<template>
+    <nav class="site-navbar" :class="'site-navbar--' + navbarLayoutType">
+        <div class="site-navbar__header">
+            <h1 class="site-navbar__brand" @click="$router.push({ name: 'home' })">
+                <a class="site-navbar__brand-lg" href="javascript:;"><img src="@/assets/img/logohead1.png" alt="" style="width: 140px;"></a>
+                <a class="site-navbar__brand-mini" href="javascript:;"><img src="@/assets/img/logoHead2.png" alt="" style="width: 47px;"></a>
+            </h1>
+        </div>
+        <div class="site-navbar__body clearfix">
+            <el-menu class="site-navbar__menu" mode="horizontal">
+                <el-menu-item class="site-navbar__switch" index="0" @click="sidebarFold = !sidebarFold">
+                    <i class="iconfont factory-shouqicaidan switching" :class="{ open: sidebarFold, packup: !sidebarFold }" style="font-size: 25px;" />
+                </el-menu-item>
+            </el-menu>
+            <el-menu class="site-navbar__menu site-navbar__menu--right" mode="horizontal">
+                <el-menu-item class="site-navbar__switch" index="4" style="padding: 8px 0;" @click.native="selectFactory">
+                    <span style=" display: block; height: 16px; padding: 0 11px; color: #00a2d4; font-size: 16px; line-height: 16px; border-left: 1px solid #d9d9d9;"><i class="iconfont factory-gongchang" style=" margin-right: 5px; color: #00a2d4;" />{{ factoryName }}</span>
+                </el-menu-item>
+            </el-menu>
+            <el-menu class="site-navbar__menu site-navbar__menu--right" mode="horizontal">
+                <el-menu-item class="site-navbar__avatar" index="3" style="padding: 0 4px;">
+                    <el-dropdown :show-timeout="0" placement="bottom">
+                        <span class="el-dropdown-link"><img src="@/assets/img/avatar.png" :alt="userName"><span>{{ realName + '(' + userName + ')' }}</span></span>
+                        <el-dropdown-menu slot="dropdown">
+                            <el-dropdown-item @click.native="updatePassword">
+                                修改密码
+                            </el-dropdown-item>
+                            <el-dropdown-item @click.native="logoutHandle()">
+                                退出
+                            </el-dropdown-item>
+                        </el-dropdown-menu>
+                    </el-dropdown>
+                </el-menu-item>
+            </el-menu>
+        </div>
+    </nav>
+</template>
+
+<script>
+import { COMMON_API } from 'common/api/api';
+
+export default {
+    name: 'MainTopbar',
+    components: {},
+    props: {
+        updatePassword: {
+            type: Function,
+            default: () => 1
+        },
+        selectFactory: {
+            type: Function,
+            default: () => {
+            //    s
+            }
+        }
+    },
+    data() {
+        return {
+            visible: false,
+            factory: JSON.parse(sessionStorage.getItem('userFactory') || '[]'),
+            factoryName: JSON.parse(sessionStorage.getItem('factory') || '{}').deptShort
+        };
+    },
+    computed: {
+        navbarLayoutType: {
+            get() {
+                return this.$store.state.common.navbarLayoutType;
+            }
+        },
+        sidebarFold: {
+            get() {
+                return this.$store.state.common.sidebarFold;
+            },
+            set(val) {
+                this.$store.commit('common/updateSidebarFold', val);
+            }
+        },
+        menuList: {
+            get() {
+                return this.$store.state.common.menuList;
+            },
+            set(val) {
+                this.$store.commit('common/updateMenuList', val);
+            }
+        },
+        mainTabs: {
+            get() {
+                return this.$store.state.common.mainTabs;
+            },
+            set(val) {
+                this.$store.commit('common/updateMainTabs', val);
+            }
+        },
+        userName: {
+            get() {
+                return this.$store.state.user.name;
+            }
+        },
+        realName: {
+            get() {
+                return this.$store.state.user.realName;
+            },
+            set(val) {
+                this.$store.commit('user/updaterealName', val);
+            }
+        }
+    },
+    methods: {
+        goEacharts() {
+            this.$router.push({
+                path: `/DataEcharts/${this.menuList
+                    .filter(item => item.type === '4')[0]
+                    .list[0].url.slice(12)
+                    .replace(/\//g, '-')}`
+            });
+        },
+        // 退出
+        logoutHandle() {
+            this.$confirm(`确定进行[退出]操作?`, '提示', {
+                confirmButtonText: '确定',
+                cancelButtonText: '取消',
+                type: 'warning'
+            })
+                .then(() => {
+                    COMMON_API.LOGOUT_API({}).then(({ data }) => {
+                        if (data && data.code === 200) {
+                            this.$cookie.delete('token');
+                            this.$router.options.isAddDynamicMenuRoutes = false;
+                            window.location.href = `${process.env.VUE_APP_HOST}`;
+                        }
+                    });
+                })
+                .catch();
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+.site-navbar {
+    background: #002140 !important;
+}
+.packup {
+    transform: rotate(0deg);
+}
+.open {
+    transform: rotate(180deg);
+}
+.switching {
+    display: block;
+    transition: 500ms;
+}
+</style>
