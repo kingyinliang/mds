b649a2a7eacb2e563ff0ce4a023a9718b1804a30
18060064 <Wyl945521?>
2020-06-01 18:10:45 +0800

    feat(test): test

---
+++ src/project/LOGIN/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="app">
+        <login />
+    </div>
+</template>
+
+<script lang="ts">
+import Login from './login.vue';
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component({
+    components: {
+        Login
+    }
+})
+export default class Index extends Vue {}
+</script>
+
+<style lang="scss">
+    html,
+    body {
+        margin: 0;
+    }
+
+    html,
+    body,
+    #app {
+        width: 100%;
+        height: 100%;
+    }
+</style>
