d4b302d7f027e9273745fa4a6dd8e906e59e42fd
16080029 <maxuehan@shinho.net.cn>
2020-07-07 09:00:59 +0800

    feat(): 杀菌

--- src/common/api/ste.ts
+++ src/common/api/ste.ts
@@ -8,7 +8,11 @@ const STE_API = {
     /**
      * @property {string} STE_HOME_LIST_API 杀菌车间-订单拆分
      */
-    STE_SPLIT_LIST_API: params => httpProxy('/steOrderSplit/query', 'POST', params)
+    STE_SPLIT_LIST_API: params => httpProxy('/steOrderSplit/query', 'POST', params),
+    /**
+     * @property {string} STE_HOME_LIST_API 杀菌车间-煮料列表-查询
+     */
+    STE_COOKING_POT_QUERY_API: params => httpProxy('/ste/steCookingPot/query', 'POST', params)
 };
 
 export default STE_API;
