0e2c07376d597abecee9dc364914a8b4aad173e7
18060064 <Wyl945521!>
2020-07-07 11:34:24 +0800

    feat(ste): 杀菌迭代任务开发

--- src/project/DFMDS/pages/Packaging/common/Material.vue
+++ src/project/DFMDS/pages/Packaging/common/Material.vue
@@ -195,21 +195,21 @@
         ruleSubmit() {
             for (const item of this.currentDataTable.filter(it => it.delFlag !== 1)) {
                 if (!item.realUseAmount) {
-                    this.$warningToast('请填写包材领用实际用量');
+                    this.$warningToast('请填写物料领用页签包材领用实际用量');
                     return false
                 }
             }
             for (const item of this.materialS.filter(it => it.delFlag !== 1)) {
                 if (!item.sterilizeStorageNo) {
-                    this.$warningToast('请填写半成品领用使用锅序');
+                    this.$warningToast('请填写物料领用页签半成品领用使用锅序');
                     return false
                 }
                 if (!item.realUsed) {
-                    this.$warningToast('请填写半成品领用实际用量');
+                    this.$warningToast('请填写物料领用页签半成品领用实际用量');
                     return false
                 }
                 if (!item.startDate) {
-                    this.$warningToast('请填写半成品领用开始使用时间');
+                    this.$warningToast('请填写物料领用页签半成品领用开始使用时间');
                     return false
                 }
             }
