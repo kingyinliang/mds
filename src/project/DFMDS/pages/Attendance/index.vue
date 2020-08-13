66bc22241629ab44645bce33f3b42fa85c8afdb6
Telliex <telliexyuzo@gmail.com>
2020-08-13 14:12:55 +0800

    docs(): 考勤管理功能还原
    考勤管理时间格式调整

--- src/project/DFMDS/pages/Attendance/index.vue
+++ src/project/DFMDS/pages/Attendance/index.vue
@@ -124,7 +124,7 @@
                             <span class="notNull">*</span>开始时间
                         </template>
                         <template slot-scope="scope">
-                            <el-date-picker v-model="scope.row.startTime" type="datetime" value-format="yyyy-MM-dd HH:mm:ss" format="yyyy-MM-dd HH:mm" placeholder="选择" size="small" :disabled="!scope.row.isRedact" style="width: 180px;" />
+                            <el-date-picker v-model="scope.row.startTime" type="datetime" value-format="yyyy-MM-dd HH:mm" format="yyyy-MM-dd HH:mm" placeholder="选择" size="small" :disabled="!scope.row.isRedact" style="width: 180px;" />
                         </template>
                     </el-table-column>
                     <el-table-column prop="dinner" width="135" label="用餐时间(MIN)" :show-overflow-tooltip="true">
@@ -140,7 +140,7 @@
                             <span class="notNull">*</span>结束时间
                         </template>
                         <template slot-scope="scope">
-                            <el-date-picker v-model="scope.row.endTime" type="datetime" value-format="yyyy-MM-dd HH:mm:ss" format="yyyy-MM-dd HH:mm" placeholder="选择" size="small" :disabled="!scope.row.isRedact" style="width: 180px;" />
+                            <el-date-picker v-model="scope.row.endTime" type="datetime" value-format="yyyy-MM-dd HH:mm" format="yyyy-MM-dd HH:mm" placeholder="选择" size="small" :disabled="!scope.row.isRedact" style="width: 180px;" />
                         </template>
                     </el-table-column>
                     <el-table-column prop="duration" min-width="80" label="时长(H)" :show-overflow-tooltip="true">
@@ -453,10 +453,23 @@
                             delIdsTemp.push(item.id)
                         }
                     } else if (item.id) {
+                        // if (!item.startTime.split(':')[2]) {
+                        //     item.startTime = item.startTime + ':00'
+                        // }
+                        // if (!item.endTime.split(':')[2]) {
+                        //     item.endTime = item.endTime + ':00'
+                        // }
+
                         if (!_.isEqual(this.orgFormDataGroup[index], item)) {
                             updateDataTemp.push(item)
                         }
                     } else {
+                        // if (!item.startTime.split(':')[2]) {
+                        //     item.startTime = item.startTime + ':00'
+                        // }
+                        // if (!item.endTime.split(':')[2]) {
+                        //     item.endTime = item.endTime + ':00'
+                        // }
                         insertDataTemp.push(item)
                     }
                 })
