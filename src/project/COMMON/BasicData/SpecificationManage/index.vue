2b56fc22b66a515a1509aabeeed8f16a136579f7
Telliex <telliexyuzo@gmail.com>
2020-09-01 16:44:06 +0800

    docs(): 基础数据/规格-代码 ts 化
    基础数据/规格-代码 ts 化

--- src/project/COMMON/BasicData/SpecificationManage/index.vue
+++ src/project/COMMON/BasicData/SpecificationManage/index.vue
@@ -17,7 +17,7 @@
                             <el-button v-if="isAuth('specInsert')" type="primary" size="small" @click="addOrupdateItem()">
                                 新增
                             </el-button>
-                            <el-button v-if="isAuth('specDel')" type="danger" size="small" :disabled="targetInfoList.length===0" @click="removeItems()">
+                            <el-button v-if="isAuth('specDel')" type="danger" size="small" :disabled="multipleSelection.length===0" @click="removeItems">
                                 批量删除
                             </el-button>
                         </el-form-item>
@@ -61,7 +61,7 @@
             </el-table>
             <el-pagination :current-page="currPage" :page-sizes="[10, 20, 50]" :page-size="pageSize" layout="total, sizes, prev, pager, next, jumper" :total="totalCount" @size-change="handleSizeChange" @current-change="handleCurrentChange" />
         </mds-card>
-        <specification-add-or-update v-if="isAddOrUpdateDailogShow" ref="SpecificationAddOrUpdate" :large-class="largeClass" :unit-class="unitClass" @refreshDataList="getItemsList" />
+        <specification-add-or-update v-if="isAddOrUpdateDailogShow" ref="SpecificationAddOrUpdate" :large-class-list="largeClassList" :unit-class-list="unitClassList" @refreshDataList="getItemsList" />
         <el-dialog title="高级查询" :close-on-click-modal="false" :visible.sync="isAdvanceSearchDailogShow" @close="closeDialog">
             <div class="formdata">
                 <el-form :model="controllableForm" size="small" label-width="110px" class="orderdialog" :rules="checkRules">
@@ -90,168 +90,206 @@
         </el-dialog>
     </div>
 </template>
-
-<script>
+<script lang="ts">
+    import { Vue, Component } from 'vue-property-decorator';
     import { COMMON_API } from 'common/api/api';
-    import SpecificationAddOrUpdate from './SpecificationAddOrUpdate';
-    export default {
-        name: 'SpecificationManage',
+    import SpecificationAddOrUpdate from './SpecificationAddOrUpdate.vue';
+
+    @Component({
+        name: 'WorkProcedureManage',
         components: {
             SpecificationAddOrUpdate
-        },
-        data() {
-            return {
-                serchSpecListObject: {},
-                unitClass: [], // 单位缓存
-                unitClassObject: {},
-                largeClass: [], // 大类缓存
-                largeClassObject: {},
-                targetInfoList: [],
-                multipleSelection: [],
-                isAddOrUpdateDailogShow: false,
-                isAdvanceSearchDailogShow: false,
-                controllableForm: {
-                    brand: '',
-                    materialCode: '',
-                    boxSpec: '',
-                    productSpec: ''
-                },
-                currPage: 1,
-                pageSize: 10,
-                totalCount: 1,
-                checkRules: {
-                    productSpec: [{ pattern: /^[1-9]\d*$/, message: '需为数字', trigger: 'blur' }
-                    ],
-                    boxSpec: [{ pattern: /^[1-9]\d*$/, message: '需为数字', trigger: 'blur' }
-                    ]
-                }
-            };
-        },
-        computed: {
-            mainClientHeight() {
-                return this.$store.state.common.mainClientHeight;
-            }
-        },
+        }
+    })
+    export default class WorkProcedureManage extends Vue {
+
+        $refs: {
+            SpecificationAddOrUpdate: HTMLFormElement;
+        }
+
+        serchSpecListObject= {}
+        unitClassList: Options[]=[] // 单位缓存
+        unitClassObject = {}
+        largeClassList: Options[]= [] // 大类缓存
+        largeClassObject= {}
+        targetInfoList: CurrentDataTable[]= []
+        multipleSelection: CurrentDataTable[]= []
+        isAddOrUpdateDailogShow= false
+        isAdvanceSearchDailogShow= false
+        controllableForm= {
+            brand: '',
+            materialCode: '',
+            boxSpec: '',
+            productSpec: ''
+        }
+
+        currPage= 1
+        pageSize= 10
+        totalCount= 1
+        checkRules= {
+            productSpec: [{ pattern: /^[1-9]\d*$/, message: '需为数字', trigger: 'blur' }
+            ],
+            boxSpec: [{ pattern: /^[1-9]\d*$/, message: '需为数字', trigger: 'blur' }
+            ]
+        }
+
+        get mainClientHeight() {
+            return this.$store.state.common.mainClientHeight;
+        }
+
         mounted() {
             this.getItemsList();
             this.getLargeClass();
             this.getUnit();
-            this.getMaterial();
-        },
-        methods: {
-            closeDialog() {
-                this.isAdvanceSearchDailogShow = false;
+        }
+
+        closeDialog() {
+            this.isAdvanceSearchDailogShow = false;
+            this.controllableForm.brand = '';
+            this.controllableForm.boxSpec = '';
+            this.controllableForm.productSpec = '';
+        }
+
+        getItemsList(haveParas = false, type = 'normal') {
+            if (haveParas) {
+                this.currPage = 1;
+            }
+            if (type === 'normal') {
                 this.controllableForm.brand = '';
                 this.controllableForm.boxSpec = '';
                 this.controllableForm.productSpec = '';
-            },
-            getItemsList(haveParas, type = 'normal') {
-                if (haveParas) {
-                    this.currPage = 1;
-                }
-                if (type === 'normal') {
-                    this.controllableForm.brand = '';
-                    this.controllableForm.boxSpec = '';
-                    this.controllableForm.productSpec = '';
-                }
+            }
 
 
-                COMMON_API.SPECS_QUERY_API({
-                    factory: JSON.parse(sessionStorage.getItem('factory') || '{}').id,
-                    boxSpec: this.controllableForm.boxSpec.trim(),
-                    bottleSpec: this.controllableForm.productSpec.trim(),
-                    brand: this.controllableForm.brand.trim(),
-                    material: this.controllableForm.materialCode.trim(),
-                    current: JSON.stringify(this.currPage),
-                    size: JSON.stringify(this.pageSize)
-                }).then(({ data }) => {
-                    if (haveParas && data.data.records.length === 0) {
-                            this.$infoToast('暂无任何内容');
-                    }
-                    this.targetInfoList = data.data.records;
-                    this.currPage = data.data.current;
-                    this.pageSize = data.data.size;
-                    this.totalCount = data.data.total;
-                    this.isAddOrUpdateDailogShow = false;
-                    this.isAdvanceSearchDailogShow = false;
-                });
-            },
-            // 新增  修改
-            addOrupdateItem(data) {
-                this.isAddOrUpdateDailogShow = true;
-                this.$nextTick(() => {
-                    this.$refs.SpecificationAddOrUpdate.init(data);
-                });
-            },
-            // 删除
-            removeItems() {
-                if (this.multipleSelection.length === 0) {
-                    this.$warningTost('请选择要删除的规格');
-                } else {
-                    this.$confirm('确认删除规格, 是否继续?', '删除规格', {
-                        confirmButtonText: '确定',
-                        cancelButtonText: '取消',
-                        type: 'warning'
-                    })
-                        .then(() => {
-                            COMMON_API.SPECS_REMOVE_API({
-                                factory: JSON.parse(sessionStorage.getItem('factory') || '{}').id,
-                                ids: this.multipleSelection
-                            }).then(() => {
-                                this.multipleSelection = [];
-                                this.$nextTick(() => {
-                                    this.getItemsList();
-                                });
+            COMMON_API.SPECS_QUERY_API({
+                factory: JSON.parse(sessionStorage.getItem('factory') || '{}').id,
+                boxSpec: this.controllableForm.boxSpec.trim(),
+                bottleSpec: this.controllableForm.productSpec.trim(),
+                brand: this.controllableForm.brand.trim(),
+                material: this.controllableForm.materialCode.trim(),
+                current: JSON.stringify(this.currPage),
+                size: JSON.stringify(this.pageSize)
+            }).then(({ data }) => {
+                if (haveParas && data.data.records.length === 0) {
+                        this.$infoToast('暂无任何内容');
+                }
+                this.targetInfoList = data.data.records;
+                this.currPage = data.data.current;
+                this.pageSize = data.data.size;
+                this.totalCount = data.data.total;
+                this.isAddOrUpdateDailogShow = false;
+                this.isAdvanceSearchDailogShow = false;
+            });
+        }
+
+        // 新增  修改
+        addOrupdateItem(data) {
+            console.log('data')
+            console.log(data)
+            this.isAddOrUpdateDailogShow = true;
+            this.$nextTick(() => {
+                this.$refs.SpecificationAddOrUpdate.init(data);
+            });
+        }
+
+        // 删除
+        removeItems() {
+            if (this.multipleSelection.length === 0) {
+                this.$warningToast('请选择要删除的规格');
+            } else {
+                this.$confirm('确认删除规格, 是否继续?', '删除规格', {
+                    confirmButtonText: '确定',
+                    cancelButtonText: '取消',
+                    type: 'warning'
+                })
+                    .then(() => {
+                        COMMON_API.SPECS_REMOVE_API({
+                            factory: JSON.parse(sessionStorage.getItem('factory') || '{}').id,
+                            ids: this.multipleSelection
+                        }).then(() => {
+                            this.multipleSelection = [];
+                            this.$nextTick(() => {
+                                this.getItemsList();
                             });
-                        })
-                        .catch();
-                }
-            },
-            // 表格选中
-            handleSelectionChange(val) {
-                this.multipleSelection = [];
-                val.forEach((item) => {
-                    this.multipleSelection.push(item.id);
-                });
-            },
-            // 大类下拉
-            getLargeClass() {
-                COMMON_API.DICTQUERY_API({
-                    dictType: 'COMMON_CATEGORY'
-                    }).then(({ data }) => {
-                        this.largeClass = data.data;
-                        this.largeClass.forEach(item => {
-                            this.largeClassObject[item.dictCode] = item.dictValue
-                        })
-                    });
-            },
-            // 单位下拉
-            getUnit() {
-                COMMON_API.DICTQUERY_API({
-                    dictType: 'COMMON_SPEC_UNIT'
-                    }).then(({ data }) => {
-                        this.unitClass = data.data;
-                        this.unitClass.forEach(item => {
-                        this.unitClassObject[item.dictCode] = item.dictValue
-                        })
-                    });
-            },
-            // 序号
-            indexMethod(index) {
-                return index + 1 + (Number(this.currPage) - 1) * Number(this.pageSize);
-            },
-            // 改变每页条数
-            handleSizeChange(val) {
-                this.pageSize = val;
-                this.getItemsList();
-            },
-            // 跳转页数
-            handleCurrentChange(val) {
-                this.currPage = val;
-                this.getItemsList();
+                        });
+                    })
+                    .catch();
             }
         }
-    };
+
+        // 表格选中
+        handleSelectionChange(val) {
+            this.multipleSelection = [];
+            val.forEach((item) => {
+                this.multipleSelection.push(item.id);
+            });
+        }
+
+        // 大类下拉
+        getLargeClass() {
+            COMMON_API.DICTQUERY_API({
+                dictType: 'COMMON_CATEGORY'
+                }).then(({ data }) => {
+                    this.largeClassList = data.data;
+                    this.largeClassList.forEach(item => {
+                        this.largeClassObject[item.dictCode] = item.dictValue
+                    })
+                });
+        }
+
+        // 单位下拉
+        getUnit() {
+            COMMON_API.DICTQUERY_API({
+                dictType: 'COMMON_SPEC_UNIT'
+                }).then(({ data }) => {
+                    this.unitClassList = data.data;
+                    this.unitClassList.forEach(item => {
+                        this.unitClassObject[item.dictCode] = item.dictValue
+                    })
+                });
+        }
+
+        // 序号
+        indexMethod(index) {
+            return index + 1 + (Number(this.currPage) - 1) * Number(this.pageSize);
+        }
+
+        // 改变每页条数
+        handleSizeChange(val) {
+            this.pageSize = val;
+            this.getItemsList();
+        }
+
+        // 跳转页数
+        handleCurrentChange(val) {
+            this.currPage = val;
+            this.getItemsList();
+        }
+    }
+
+interface CurrentDataTable {
+    bottleSpec?: number;
+    bottleSpecUnit?: string;
+    boxSpec?: number;
+    boxSpecUnit?: string;
+    brand?: string;
+    changer?: string;
+    factory?: string;
+    id?: string;
+    largeClass?: string;
+    materialCode?: string;
+    materialName?: string;
+    version?: number;
+}
+
+interface Options {
+    dictCode: string;
+    dictId?: string;
+    dictValue: string;
+    factoryName?: string;
+    id?: string;
+}
+
 </script>
 
 <style scoped></style>
