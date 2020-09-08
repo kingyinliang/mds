543e341ac0ad5c36d6461b2c48bcc70200ba13d8
Telliex <telliexyuzo@gmail.com>
2020-09-08 15:37:54 +0800

    refactor():  组件重构
    弹窗 & 展示用表格组件重构

--- src/components/TableShow.vue
+++ src/components/TableShow.vue
@@ -3,17 +3,20 @@
  * @Anthor: Telliex
  * @Date: 2020-09-02 10:21:15
  * @LastEditors: Telliex
- * @LastEditTime: 2020-09-04 14:29:04
+ * @LastEditTime: 2020-09-08 15:24:05
 -->
 <template lang="pug">
     el-table(ref="targetTable" class="newTable" border header-row-class-name="tableHead" :data="targetTable" :height="tableElementSetting.props.height" tooltip-effect="dark" style="width: 100%;" @selection-change="handleSelectionChange")
         el-table-column(v-if="targetTable.length!==0" type="selection" width="50" fixed)
         el-table-column(type="index" label="序号" :index="indexMethod" width="55" align="center" fixed)
         template(v-for="(tableElement,index) in tableElementSetting.data")
-            el-table-column(v-if="tableElement.type==='multiple'" :key="'targetTable'+tableElement.prop+index" :prop="tableElement.prop" :label="tableElement.label" :show-overflow-tooltip="true" :min-width="tableElement.minWidth" :width="tableElement.width")
+            el-table-column(v-if="tableElement.type==='multiple'" :key="'targetTable'+tableElement.prop+index" :prop="tableElement.prop" :label="tableElement.label" :show-overflow-tooltip="true" :min-width="tableElement.minWidth" :width="tableElement.width" :align="tableElement.align || 'left'" :header-align="tableElement.headerAlign || 'left'")
                 template(slot-scope="scope")
-                    span(v-for="val in tableElement.content" :key="'targetTable'+val") {{ scope.row[tableElement.prop] }}
-            el-table-column(v-else-if="tableElement.type==='single'" :key="'targetTable'+tableElement.prop+index" :prop="tableElement.prop" :label="tableElement.label" :min-width="tableElement.minWidth" :width="tableElement.width" :show-overflow-tooltip="true")
+                    span(v-for="val in tableElement.content" :key="'targetTable'+val" style="padding-right:5px") {{ scope.row[val] }}
+            el-table-column(v-else-if="tableElement.type==='single'&&tableElement.wrapper" :key="'targetTable'+tableElement.prop+index" :prop="tableElement.prop" :label="tableElement.label" :min-width="tableElement.minWidth" :width="tableElement.width" :show-overflow-tooltip="true" :align="tableElement.align || 'left'" :header-align="tableElement.headerAlign || 'left'")
+                template(slot-scope="scope") {{ tableElement.wrapper[scope.row[tableElement.prop]] }}
+            el-table-column(v-else-if="tableElement.type==='single'" :key="'targetTable'+tableElement.prop+index" :prop="tableElement.prop" :label="tableElement.label" :min-width="tableElement.minWidth" :width="tableElement.width" :show-overflow-tooltip="true" :align="tableElement.align || 'left'" :header-align="tableElement.headerAlign || 'left'")
+                template(slot-scope="scope")  {{ scope.row[tableElement.prop] }}
             el-table-column(v-else-if="tableElement.type==='button'" :key="'targetTable'+tableElement.prop+index" :min-width="tableElement.minWidth" :width="tableElement.width" :label="tableElement.label" fixed="right")
                 template(slot-scope="scope")
                     el-button(v-for="(val,valIndex) in tableElement.content" :key="'targetTable'+val.buttonName+valIndex" :class="val.btn" :icon="val.icon" style="padding: 0;" type="text" @click="btnUpdateItem(scope.row)")
@@ -35,9 +38,13 @@
             targetTable: HTMLFormElement;
         }
 
-        multipleSelection= []
+        multipleSelection: DelIdList[]= []
         currPage= 1
         pageSize= 10
+
+        created() {
+            this.setting()
+        }
 
         init() {
             //
@@ -61,8 +68,33 @@
 
         // 获取勾选项
         getMultipleSelection() {
-            return this.multipleSelection
+            const tempMultipleSelection: string[] = []
+            this.multipleSelection.forEach((item) => {
+                tempMultipleSelection.push(item.id);
+            });
+
+            return tempMultipleSelection
         }
+
+        setting() {
+            this.tableElementSetting.data.forEach(item => {
+                if (item.wrapperList) {
+                    this.$set(item, 'wrapper', item.wrapperList);
+                } else if (item.wrapperFn) {
+                    item.wrapperFn().then((res) => {
+                        this.$set(item, 'wrapper', res);
+                    });
+                }
+            })
+        }
+
+        btnUpdateItem(val) {
+            this.$emit('updateItem', val)
+        }
+    }
+
+    interface DelIdList {
+        id: string;
     }
 </script>
 <style lang="sass" scoped></style>
