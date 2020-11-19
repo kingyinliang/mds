67f20512dbc2c13e2ec40a0c6ca79c387861cfe1
kingyinliang <w1790353735@qq.com>
2020-11-19 14:59:33 +0800

    feat(packaging): 包装新需求添加

---
+++ src/project/DFMDS/pages/Fermentation/DeploySauce/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div />
+</template>
+
+<script lang="ts">
+    import { Vue, Component } from 'vue-property-decorator';
+
+    @Component({})
+    export default class DeploySauce extends Vue {
+    }
+</script>
+
+<style scoped>
+
+</style>
