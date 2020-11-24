e7cfb8e5a87cd510c040b50ad267f03eadcd7fe2
16080029 <maxuehan@shinho.net.cn>
2020-11-24 11:00:02 +0800

    modify(): 接口传参类型调整

--- src/project/DFMDS/pages/KojiMaking/WorkingProcedure/SteamedBean.vue
+++ src/project/DFMDS/pages/KojiMaking/WorkingProcedure/SteamedBean.vue
@@ -205,7 +205,7 @@
             COMMON_API.HOLDER_DROPDOWN_API({
                 deptId: this.formHeader.workShop,
                 factory: JSON.parse(sessionStorage.getItem('factory') || '{}').id,
-                holderType: '025'
+                holderType: ['025']
             }).then(({ data }) => {
                 this.scanList = data.data || [];
             });
