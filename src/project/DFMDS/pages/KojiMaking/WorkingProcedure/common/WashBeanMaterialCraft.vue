e6d375f5f9ba20144574843b464eb253b50f2efb
Telliex <telliexyuzo@gmail.com>
2021-01-05 17:52:23 +0800

    fix(): debug DFMDS-3088
    debug DFMDS-3088 制曲车间/洗豆工序|工艺控制:筛豆记录的杂质没有单位

--- src/project/DFMDS/pages/KojiMaking/WorkingProcedure/common/WashBeanMaterialCraft.vue
+++ src/project/DFMDS/pages/KojiMaking/WorkingProcedure/common/WashBeanMaterialCraft.vue
@@ -77,7 +77,7 @@
                 </el-table-column>
                 <el-table-column label="单位">
                     <template slot-scope="scope">
-                        {{ scope.row.unitName }}
+                        {{ transferUnit(scope.row.unit) }}
                     </template>
                 </el-table-column>
                 <el-table-column width="140" show-overflow-tooltip>
@@ -337,6 +337,9 @@
         // 泡豆罐操作对象
         scanRow: Craft = {};
 
+        // 单位清单
+        unitList: Options[]=[]
+
         get filterMaterialTableData() {
             const TEMObject = {};
             return this.setMaterialTableData.reduce((totalArr, currentItem) => {
@@ -371,12 +374,6 @@
                 item.kojiOrderNo = formHeader.kojiOrderNo;
                 item.orderNo = formHeader.orderNo;
             });
-            console.log(1111111);
-
-            console.log(this.temCraftWashBeanTable)
-            console.log(this.craftWashBeanTable);
-
-
             return {
                 kojiBeanSieveSaveDto: {
                     ...this.craftSeiveBeanInfo,
@@ -417,13 +414,15 @@
         ];
 
         // 初始化数据
-        init(formHeader) {
+        async init(formHeader) {
             const { orderNo, workShop } = formHeader;
             this.formHeader = formHeader;
-            // 查询设备list
-            this.getSieveDeviceList(workShop);
+            // 查询设备 list
+            await this.getSieveDeviceList(workShop);
+            // 查询单位 list
+            await this.getUnitList();
             // 查询筛豆记录
-            this.getSeiveBeanLogList(formHeader);
+            await this.getSeiveBeanLogList(formHeader);
             // 查询洗豆记录
             this.getWashBeanLogList(formHeader);
             // 查询审核记录
@@ -469,6 +468,7 @@
                 sieveImpurityType: '',
                 sieveMans: '',
                 unit: 'KG',
+                unitName: '千克',
                 remark: '',
                 changed: dateFormat(new Date(), 'yyyy-MM-dd hh:mm:ss'),
                 changer: getUserNameNumber(),
@@ -714,11 +714,27 @@
             });
         }
 
-        // 查询设备list
+        // 查询设备 list
         getSieveDeviceList(deptId) {
             COMMON_API.DEVICE_LISTBYTYPE_API({ deptId, deptName: '洗豆', factory: JSON.parse(sessionStorage.getItem('factory') || '{}').id }).then(({ data }) => {
                 this.sieveDeviceList = data.data || [];
             });
+        }
+
+        // 查询单位 list
+        getUnitList() {
+            COMMON_API.DICTQUERY_API({
+                dictType: 'COMMON_UNIT'
+            }).then(({ data }) => {
+                console.log('单位')
+                console.log(data.data)
+                this.unitList = data.data;
+            });
+        }
+
+        transferUnit(val) {
+            // return val
+            return this.unitList.filter(item => item.dictCode === val)[0].dictValue || val
         }
     }
 
@@ -779,6 +795,14 @@
         sieveDeviceName?: string;
         deviceNo?: string;
         deviceName?: string;
+    }
+    interface Options{
+        dictCode?: string;
+        dictId?: string;
+        dictOrder?: string;
+        dictValue?: string;
+        factoryName?: string;
+        id?: string;
     }
 </script>
 
