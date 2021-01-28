99384312a76ae012cc1246c47e8be982d7e69619
zhoufx <zhoufuxin@shinho.net.cn>
2021-01-28 09:49:30 +0800

    docs(): 报表功能开发

--- src/project/DFMDS/pages/ReportForms/Packaging/SumOfSubItems/index.vue
+++ src/project/DFMDS/pages/ReportForms/Packaging/SumOfSubItems/index.vue
@@ -1,3 +1,4 @@
+<!-- 分品项产量汇总  -->
 <template>
     <div class="header_main">
         <query-table
@@ -5,15 +6,15 @@
             :show-table="true"
             :show-index-column="false"
             :column="column"
-            :is-show-summary="true"
-            :get-summaries="getSummaries"
-            :show-page="false"
+            :show-page="true"
             query-auth=""
             :query-form-data="queryFormData"
             :list-interface="listInterface"
+            :get-summaries="getSummaries"
             :custom-data="true"
             :factory-type="1"
             :export-excel="true"
+            :is-show-summary="true"
             @get-data-success="setData"
         />
     </div>
@@ -21,50 +22,65 @@
 
 <script lang="ts">
     import { Vue, Component } from 'vue-property-decorator';
-    import { COMMON_API } from 'common/api/api';
+    import { COMMON_API, REPORTS_API } from 'common/api/api';
     // import { dateFormat } from 'utils/utils';
 
     @Component({
         components: {
         },
-        name: 'OutputSummary'
+        name: 'SumOfSubItems'
     })
-    export default class OutputSummary extends Vue {
+    export default class SumOfSubItems extends Vue {
         //表格数据
         column = [
             {
                 prop: 'workShopName',
-                label: '生产车间',
+                label: '品项',
                 minWidth: '120'
             },
             {
-                prop: 'unitName',
-                label: '单位',
+                prop: 'holderNo',
+                label: '规格',
                 minWidth: '120'
             },
             {
                 prop: 'holderTypeName',
-                label: '年度',
+                label: '产出',
+                subLabel: '（箱）',
                 minWidth: '120'
             },
             {
                 prop: 'holderNo',
-                label: '月/季',
+                label: '产出',
+                subLabel: '（吨）',
                 minWidth: '120'
             },
             {
                 prop: 'holderNo',
-                label: '有效产能',
+                label: '工作天数',
                 minWidth: '120'
             },
             {
                 prop: 'holderNo',
-                label: '实际产能',
+                label: '总班次',
                 minWidth: '120'
             },
             {
                 prop: 'holderNo',
-                label: '产能利用率',
+                label: '人工工时',
+                subLabel: '（H）',
+                minWidth: '120'
+            },
+            {
+                prop: 'holderNo',
+                label: '合计',
+                subLabel: '（箱）',
+                minWidth: '120'
+            },
+            {
+                prop: 'holderNo',
+                label: '合计',
+                subLabel: '（吨）',
                 minWidth: '120'
             }
         ];
@@ -76,20 +92,11 @@
         // 查询表头
         queryFormData = [
             {
-                type: 'radio',
-                prop: 'aaaaaaa',
-                radioArr: [
-                    { label: '月报', val: '月报val' },
-                    { label: '季报', val: '季报val' }
-                ],
-                rule: [{ required: true, message: '请选择月报/季报' }]
-            },
-            {
                 type: 'select',
                 label: '生产车间',
-                labelWidth: '120',
                 prop: 'workShop',
                 defaultValue: '',
+                labelWidth: '100',
                 rule: [{ required: true, message: '请选择生产车间', trigger: 'blur' }],
                 defaultOptionsFn: () => {
                     return COMMON_API.ORG_QUERY_WORKSHOP_API({
@@ -106,61 +113,62 @@
             },
             {
                 type: 'date-picker',
-                label: '年度',
-                prop: 'oneorderProductDate',
-                dataType: 'year',
+                label: '生产月',
+                dataType: 'month',
                 defaultValue: '',
                 labelWidth: '100',
-                rule: [{ required: true, message: '请选择年度', trigger: 'blur' }],
-                valueFormat: 'yyyy'
+                rule: [{ required: true, message: '请选择月', trigger: 'blur' }],
+                prop: 'oneorderProductDate',
+                valueFormat: 'MM'
             }
         ];
 
         // 查询请求
         listInterface = params => {
             params.factory = JSON.parse(sessionStorage.getItem('factory') || '{}').id;
-            // return REPORTS_API.REPORT_PACKAGING_OEE_API(params);
-            return new Promise(resolve => {
-                resolve({
-                    data: {
-                        data: {
-                            records: [
-                                { workShopName: '车间一', unitName: '单位' }
-                            ]
-                        }
-                    }
-                })
-            })
+            return REPORTS_API.REPORT_PACKAGING_OEE_API(params);
+        };
+
+        /**
+         * @description: data 表单 合计
+         * @param1 {*}
+         * @param2 {*}
+         * @param3 {*}
+         * @return {*}
+         * @example: 示例代码
+         * @param {*} getSummaries
+         */
+        getSummaries = param => {
+            const { columns, data } = param;
+            const sums: string[] = []
+            console.log('表单合计param')
+            console.log(param)
+            columns.forEach((column, index) => {
+            if (index === 0) {
+                sums[index] = '合计';
+                return;
+            }
+            const values = data.map(item => Number(item[column.property]));
+            if (!values.every(value => isNaN(value))) {
+                sums[index] = values.reduce((prev, curr) => {
+                const value = Number(curr);
+                if (!isNaN(value)) {
+                    return prev + curr;
+                }
+                    return prev;
+
+                }, 0);
+                sums[index] += ' 元';
+            } else {
+                sums[index] = 'N/A';
+            }
+            });
+            return sums
         };
 
         // 设置数据
         setData(data) {
             console.log(data);
-        }
-
-        getSummaries(param) {
-            const { columns, data } = param;
-            const sums: string[] = [];
-            columns.forEach((column, index: number) => {
-                if (index === 0) {
-                    sums[index] = '合计';
-                    return;
-                }
-                const values = data.map(item => Number(item[column.property]));
-                if (!values.every(value => isNaN(value))) {
-                    sums[index] = values.reduce((prev, curr) => {
-                        const value = Number(curr);
-                        if (!isNaN(value)) {
-                            return prev + curr;
-                        }
-                        return prev;
-                    }, 0);
-                    sums[index] += ' 元';
-                } else {
-                    sums[index] = 'N/A';
-                }
-            });
-            return sums;
         }
 
     }
