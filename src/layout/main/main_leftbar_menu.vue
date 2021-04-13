e8d984a705b50ed7b098e0031a62d12ef71ecfdc
kingyinliang <17600813147@163.com>
2021-04-13 19:42:49 +0800

    feat(sso): sso

--- src/layout/main/main_leftbar_menu.vue
+++ src/layout/main/main_leftbar_menu.vue
@@ -66,9 +66,13 @@ export default {
     methods: {
         // 通过menuId与动态(菜单)路由进行匹配跳转至指定路由
         gotoRouteHandle(menu) {
-            const route = this.dynamicMenuRoutes.filter(item => item.meta.menuId === menu.id);
-            if (route.length >= 1) {
-                this.$router.push({ path: route[0].path });
+            if (/^http[s]?:\/\/.*/.test(menu.menuUrl)) {
+                window.location.href = menu.menuUrl + '&token=' + this.$cookie.get('token') + '&tenant=MDS'
+            } else {
+                const route = this.dynamicMenuRoutes.filter(item => item.meta.menuId === menu.id);
+                if (route.length >= 1) {
+                    this.$router.push({ path: route[0].path });
+                }
             }
         }
     }
