91a6edf2561124e4c3d527d8ae821bb6155269f4
kingyinliang <17600813147@163.com>
2021-04-21 20:12:10 +0800

    feat(sso): sso优化

--- src/utils/SSOLogin.ts
+++ src/utils/SSOLogin.ts
@@ -2,6 +2,7 @@ import SSOLogin from '@shinho-fe/ssologin'
 // import SSOLogin from './net/SSOLogin.min'
 
 export default new SSOLogin({
+    NODE_ENV: process.env.NODE_ENV,
     host: process.env.VUE_APP_SYSTEM_API + process.env.VUE_APP_API_V,
     tenant: 'MDS',
     clientId: 'bab8aedd8f0111eb9c21026438001fa4',
