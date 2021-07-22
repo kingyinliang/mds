1ad1a9945dccacf051d57f745471d9e547d2b8b9
kingyinliang <17600813147@163.com>
2021-07-22 10:18:22 +0800

    feat(deployment): 联调原汁组和待调配列表

--- src/project/MDS/api/api.js
+++ src/project/MDS/api/api.js
@@ -1627,6 +1627,9 @@ export const STERILIZED_API = {
      */
     WAITDEPLOYMENTLIST_API: HOST + '/ste/order/orderList',
     NEW_WAITDEPLOYMENTLIST_API: HOST + '/ste/order/v2/toBeAllocateList',
+    NEW_WAITDEPLOYMENT_MATERIAL_LIST_API: HOST + '/ste/allocate/v2/allocateMaterialQuery',
+    NEW_WAITDEPLOYMENT_MATERIAL_SAVE_API: HOST + '/ste/allocate/v2/allocateSave',
+    NEW_WAITDEPLOYMENT_MATERIAL_UPDATE_API: HOST + '/ste/allocate/v2/allocateOrderAdjust',
     NEW_WAITDEPLOYMENT_API: HOST + '/ste/order/v2/toBeAllocateStayList',
     WAITDEPLOYMENTMATERIALISTAPI: HOST + '/ste/order/productsList',
     /**
@@ -1672,6 +1675,7 @@ export const STERILIZED_API = {
     JUICEDEPLOYMENTLIST: HOST + '/ste/allocate/allocateList',
     JUICEDEPLOYMENTLISTTP: HOST + '/ste/allocate/allocateListTp',
     JUICEDEPLOYMENTCREATE: HOST + '/ste/allocate/allocatecreate',
+    NEW_JUICEDEPLOYMENTCREATE: HOST + '/ste/allocate/v2/allocateCreate',
     JUICEDEPLOYMENTCREATETP: HOST + '/ste/allocate/allocateCreateTp',
     /**
      *  工艺控制提交
